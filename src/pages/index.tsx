4288edccb62449a47fb9e8c9a26a3b929e2f2f68
riotgoools <91990710+riotgoools@users.noreply.github.com>
2024-01-28 15:10:34 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,28 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Script from 'next/script';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [randomImageNumber, setRandomImageNumber] = useState(1);
 
   useEffect(() => {
-    // Generate a random number between 1 and 100 (you can adjust the range as needed)
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const randomNumber = Math.floor(Math.random() * 156) + 1;
     setRandomImageNumber(randomNumber);
-  }, []); // Run this effect only once when the component mounts
+  }, []); 
 
   return (
     <>
       <Head>
-        <meta property="og:title" content="Tarot frame" />
-        <meta property="og:image" content="tarot-frame.vercel.app/${randomImageNumber}.png" />
+        <meta property="og:title" content="Tarot Frame" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="tarot-frame.vercel.app/${randomImageNumber}.png" />
         <meta property="fc:frame:button:1" content="Pull" />
       </Head>
+      <Script id="dynamic-meta">
+        {`
+          document.head.querySelector('meta[property="og:image"]').content = 'https://tarot-frame.vercel.app/${randomImageNumber}.png';
+          document.head.querySelector('meta[property="fc:frame:image"]').content = 'https://tarot-frame.vercel.app/${randomImageNumber}.png';
+        `}
+      </Script>
     </>
-  )
+  );
 }
