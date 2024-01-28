999c3ed272021b3e93dec27c711960000df79e61
riotgoools <91990710+riotgoools@users.noreply.github.com>
2024-01-28 15:23:00 -0700

    img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <>
       <Head>
         <meta property="og:title" content="Tarot Frame" />
-        <meta property="og:image" content="https://tarot-frame.vercel.app/molemon01.png" />
+        <meta property="og:image" content="https://tarot-frame.vercel.app/1.png" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://tarot-frame.vercel.app/molemon01.png" />
+        <meta property="fc:frame:image" content="https://tarot-frame.vercel.app/1.png" />
         <meta property="fc:frame:button:1" content="Pull Card" />
         <meta property="fc:frame:post_url" content="https://tarot-frame.vercel.app/api/post" />
       </Head>
--- src/utils.ts
+++ src/utils.ts
@@ -8,9 +8,8 @@ export function generateFarcasterFrame(image: string, choice: number) {
     <head>
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="${image}" />
+      <meta property="fc:frame:button:1" content="Pull Card" />
       <meta property="fc:frame:post_url" content="https://elle-longcasting-frame.vercel.app/api/post" />
-      <meta property="fc:frame:button:2" content="Ineffective!" />
-
     </head>
     <body>
       
