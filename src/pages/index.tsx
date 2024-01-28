c43231f32427130decd7e1fd98c0fd5b2dcdc9e0
riotgoools <91990710+riotgoools@users.noreply.github.com>
2024-01-28 15:20:26 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,14 @@
 import Head from 'next/head'
 
 export default function Home() {
-  // Generate a random number between 1 and 100 (you can adjust the range as needed)
-  const randomImageNumber = Math.floor(Math.random() * 156) + 1;
-
-export default function Home() {
   return (
     <>
       <Head>
         <meta property="og:title" content="Tarot Frame" />
-        <meta property="og:image" content="https://tarot-frame.vercel.app/1.png" />
+        <meta property="og:image" content="https://tarot-frame.vercel.app/molemon01.png" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://tarot-frame.vercel.app/1.png" />
-        <meta property="fc:frame:button:1" content="Pull" />
+        <meta property="fc:frame:image" content="https://tarot-frame.vercel.app/molemon01.png" />
+        <meta property="fc:frame:button:1" content="Pull Card" />
         <meta property="fc:frame:post_url" content="https://tarot-frame.vercel.app/api/post" />
       </Head>
     </>
