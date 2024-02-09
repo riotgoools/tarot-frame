90ce5f404b1f16220dd75ca99999561db5d828cb
riotgoools <91990710+riotgoools@users.noreply.github.com>
2024-02-08 20:09:50 -0700

    aspect ratio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,10 @@ export default function Home() {
     <>
       <Head>
         <meta property="og:title" content="Tarot Frame" />
-        <meta property="og:image" content="https://elle-tarot-frame.vercel.app/0.png" />
+        <meta property="og:image" content="https://elle-tarot-frame.vercel.app/0wide.png" />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="https://elle-tarot-frame.vercel.app/0.png" />
+        <meta property="fc:frame:image:aspect_ratio" content="1:1" />
         <meta property="fc:frame:button:1" content="Pull Card" />
         <meta property="fc:frame:post_url" content="https://elle-tarot-frame.vercel.app/api/post" />
         
--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,7 @@ export function pullTarotCard(image: string) {
     <head>
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="${image}" />
+      <meta property="fc:frame:image:aspect_ratio" content="1:1" />
       <meta property="fc:frame:button:1" content="Pull Card" />
       <meta property="fc:frame:post_url" content="https://elle-tarot-frame.vercel.app/api/post" />
     </head>
